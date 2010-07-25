fe03f3bdf89d0ca1d3c8ad7597669192d70a1e4c
Hanno Schlichting <hanno@hannosch.eu>
2010-07-25 09:50:47 +0000

    Prepare Products.OFSP 2.13.2.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Products.OFSP',
-      version = '2.13.2dev',
+      version = '2.13.2',
       url='http://pypi.python.org/pypi/Products.OFSP',
       license='ZPL 2.1',
       description="General Zope 2 help screens.",
